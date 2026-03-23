09847d9ad9566993a9e28bfb65e2f5100d2d2767
Erica Qi <eqi@charlotte.edu>
2026-03-23 13:09:41 -0400

    Refactor sandwich creation for improved readability

--- Assignment5/api/controllers/controllers/sandwiches.py
+++ Assignment5/api/controllers/controllers/sandwiches.py
@@ -1,24 +1,24 @@
-from sqlalchemy.orm import Session
+# sandwiches.py
 from fastapi import Response, status
+from sqlalchemy.orm import Session
 from .. import models, schemas
 
-# CREATE
 def create(db: Session, sandwich: schemas.SandwichCreate):
-    db_sandwich = models.Sandwich(name=sandwich.name, price=sandwich.price)
+    db_sandwich = models.Sandwich(
+        name=sandwich.name,
+        price=sandwich.price
+    )
     db.add(db_sandwich)
     db.commit()
     db.refresh(db_sandwich)
     return db_sandwich
 
-# READ ALL
 def read_all(db: Session):
     return db.query(models.Sandwich).all()
 
-# READ ONE
 def read_one(db: Session, sandwich_id: int):
     return db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id).first()
 
-# UPDATE
 def update(db: Session, sandwich_id: int, sandwich: schemas.SandwichUpdate):
     db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
     update_data = sandwich.dict(exclude_unset=True)
@@ -26,7 +26,6 @@ def update(db: Session, sandwich_id: int, sandwich: schemas.SandwichUpdate):
     db.commit()
     return db_sandwich.first()
 
-# DELETE
 def delete(db: Session, sandwich_id: int):
     db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
     db_sandwich.delete(synchronize_session=False)
